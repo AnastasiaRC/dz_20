13d369f94b0301a4a4f1cdf190e0f773a44d5c2d
анастасия рябцева <nastia.riabtseva18@gmail.com>
2023-03-05 19:25:49 +0300

    First commit

---
+++ dao/model/director.py
@@ -0,0 +1,14 @@
+from dz_20.setup_db import db
+from marshmallow import Schema, fields
+
+
+class Director(db.Model):
+    __tablename__ = 'director'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+
+class DirectorSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
